0a835d91ee3e2baa5bb5e7676d83417fbf433c19
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-05-03 12:27:37 -0700

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,12 +1,59 @@
 import android.content.Intent
 import android.content.SharedPreferences
+import android.media.MediaRecorder
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceManager
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var mediaRecorder: MediaRecorder
+    private lateinit var audioFile: File
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        if (!hasOverlayPermission()) {
+            requestOverlayPermission()
+        }
+
+        setupSettingsButton()
+        setupAudioRecorder()
+    }
+
+    private fun setupSettingsButton() {
+        findViewById<Button>(R.id.settingsButton).setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+    }
+
+    private fun setupAudioRecorder() {
+        mediaRecorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            audioFile = File.createTempFile("temp_audio", ".3gp", externalCacheDir)
+            setOutputFile(audioFile.absolutePath)
+        }
+    }
+
+    private fun startRecording() {
+        mediaRecorder.prepare()
+        mediaRecorder.start()
+    }
+
+    private fun stopRecording() {
+        mediaRecorder.stop()
+        mediaRecorder.reset()
+        setupAudioRecorder()
+    }
 
     private lateinit var sharedPreferences: SharedPreferences
 
